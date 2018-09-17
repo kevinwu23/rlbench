d92ed0ac2b415c0bffcd0151ce17c5eef46a95d4
Kevin Wu <kevinwu@Kevins-MacBook-Pro-3.local>
2018-09-17 03:36:50 -0400

    rl_runscript.sh

--- kevin_deeprl/rl_runscript.sh
+++ kevin_deeprl/rl_runscript.sh
@@ -3,7 +3,6 @@
 #SBATCH -n 4                    # Number of cores
 #SBATCH -N 1                    # Ensure that all cores are on one machine
 #SBATCH -t 4-12:00              # Runtime in D-HH:MM
-#SBATCH -p gpu       			# Partition to submit to
 #SBATCH --mem=64000             # Memory pool for all cores (see also --mem-per-cpu)
 #SBATCH -o output/butterflies-levelswitch-reset3.out      # File to which STDOUT will be written
 #SBATCH -e errors/butterflies-levelswitch-reset3.err      # File to which STDERR will be written
@@ -16,3 +15,5 @@ python main.py -game_name gvgai-butterflies -doubleq 1 -trial_num 1 -level_switc
 # python main.py -game_name gvgai-zelda -doubleq 1 -trial_num 12 -pretrain 0 -level_switch other
 # python main.py -game_name gvgai-portals -doubleq 1 -trial_num 12 -lr .0001 -pretrain 0 -level_switch other
 # python main.py -game_name seaquest -doubleq 1 -trial_num 12 -lr .00001 -pretrain 0 -level_switch other
+
+##SBATCH -p gpu       			# Partition to submit to
